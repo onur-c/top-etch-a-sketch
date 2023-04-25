dd4c8d8d55960886facdea789a16745e04c0eb4f
Onur Çelik <onurccelik1@gmail.com>
2023-04-25 15:00:58 +0300

    Add user input for grid size customization

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
 
-for (let i = 0; i < 16; i++) {
+let buttonDiv = document.createElement("div");
 
-    const container = document.createElement("container");
-    container.className = "flex-container";
+let inputButton = document.createElement("button");
 
-    for ( let j = 0 ; j < 16; j++){ 
+buttonDiv.className = "button-div";
 
-        const gridDiv = document.createElement("div");
-        gridDiv.className = "grid-item";    
-        container.appendChild(gridDiv); 
+inputButton.className = "input-button";
 
-    }
+inputButton.innerText = "Set square amount";
+
+buttonDiv.appendChild(inputButton);
+
+document.body.appendChild(buttonDiv);
+
+var input = 16;
+buildGrid(input);
+
+inputButton.addEventListener("click",()=>{
+    removeLastGrid();
+
+    input = prompt("How many squares each side ?");
     
-document.body.appendChild(container);
+    buildGrid(input);
+})
+
+
+function buildGrid(size){
+    const grid = document.createElement("div");
+    grid.id ="grid-"+input;
     
+    for (let i = 0; i < size; i++) {
+
+        const container = document.createElement("container");
+        container.className = "flex-container";
+    
+        for ( let j = 0 ; j < size; j++){ 
+    
+            const gridDiv = document.createElement("div");
+            gridDiv.className = "grid-item";    
+            container.appendChild(gridDiv); 
+    
+        }
+    
+    grid.appendChild(container);
+        
+    } 
+    document.body.appendChild(grid);   
 }
 
+
+
+function removeLastGrid(){
+    document.body.removeChild(document.getElementById("grid-"+input));
+}
 
 
 
